11e36753507a86f72ed1353d608d8bb33a353836
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-22 16:28:27 +0530

    feat: add user signup logic with validation and hashing
    - Added signup logic using bcrypt for password hashing.
    - Included validation to ensure password length is at least 6 characters.
    - Checked for existing users by email to prevent duplicate accounts.
    - Placeholder added for JWT generation after successful signup.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,32 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, passseord } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at 6 characters" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      passowrd: hashedPassword,
+    });
+
+    if (newUser) {
+      //genrate jwt token here
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
