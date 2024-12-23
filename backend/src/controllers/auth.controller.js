5cb7f1d15fada31c3106e537ec4a3296abcf7159
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-23 09:59:03 +0530

    Enhanced signup functionality with token generation and error handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  const { fullName, email, passseord } = req.body;
+  const { fullName, email, password } = req.body;
   try {
     if (password.length < 6) {
       return res
@@ -18,15 +19,27 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       email,
-      passowrd: hashedPassword,
+      password: hashedPassword,
     });
 
     if (newUser) {
-      //genrate jwt token here
+      //Generate jwt token here
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
